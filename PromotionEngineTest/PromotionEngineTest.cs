63067133a30e749a0be5afe0f89dfb7aaf1ae720
Vikrant Vaibhav <vikrant.vaibhav@cognizant.com>
2020-12-09 23:58:16 +0530

    Adding Test Project

---
+++ PromotionEngineTest/PromotionEngineTest.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace PromotionEngineTest
+{
+    [TestFixture]
+    public class PromotionEngineTest
+    {
+        [Test]
+        public void FixedPriceTest()
+        {
+            
+        }
+    }
+}
